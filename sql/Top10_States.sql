e69eb2aa815249db936f6e07ecdf48c9ce2367d5
geethaputta58 <sirigeethap@gmail.com>
2026-07-07 03:45:08 +0530

    Create Top10_States.sql

---
+++ sql/Top10_States.sql
@@ -0,0 +1,7 @@
+SELECT
+    State,
+    ROUND(SUM(Consumption), 2) AS Total_Consumption
+FROM `power-consumption-analysis.electricity.power_consumption_clean`
+GROUP BY State
+ORDER BY Total_Consumption DESC
+LIMIT 10;
